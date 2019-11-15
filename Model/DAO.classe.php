63a8a74e8337269931ff4012268635815882ac25
Ng-B <naodg68@gmail.com>
2019-11-15 14:07:34 +0100

    ef

--- Model/DAO.classe.php
+++ Model/DAO.classe.php
@@ -13,15 +13,38 @@
       }
    }
 
-   public function get(int $id): Music {
-     $reponse=$this->db->query("SELECT * FROM music WHERE id=$id  ");
-     $donnees=$reponse-> fetchAll(PDO::FETCH_CLASS,"Music");
-     $musique= $donnees[0];
-     return $musique;
+   public function getAdherentParNom(string $nom): array() {
+     $reponse=$this->db->query("SELECT * FROM ADHERENT WHERE Nom LIKE '%$nom' ");
+     $donnees=$reponse-> fetchAll(PDO::FETCH_CLASS,"Adherent");
+     if(count($donnees)>1) {
+       for(int $i=0; $i<count($donnees); $i++) {
+         $adherent[$i] = $donnees[$i];
+       }
+       return $adherent[];
+     }
+     else if(count($donnees) == 0) {
+       printf("Aucun adherent avec ce nom");
+     } else {
+       $adherent[]= $donnees[0];
+       return $adherent[];
+     }
    }
 }
 
-
+public function getAdherentParId(int $id): Adherent {
+  $reponse=$this->db->query("SELECT * FROM ADHERENT WHERE id=$id ");
+  $donnees=$reponse-> fetchAll(PDO::FETCH_CLASS,"Adherent");
+  if(count($donnees)>1) {
+    printf("Plusieurs adherents avec cet id");
+  }
+  else if(count($donnees) == 0) {
+    printf("Aucun adherent avec cet id");
+  } else {
+    $adherent= $donnees[0];
+  }
+  return $adherent;
+ }
+}
 
 
 
