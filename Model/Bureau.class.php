1ebb8c2b9a24aa77be17705e711974d445e2c218
Raymond <locatean@hostname.iut2.upmf-grenoble.fr>
2019-12-20 16:26:07 +0100

    hùofphvsùwptmjgOayip j ghl

---
+++ Model/Bureau.class.php
@@ -0,0 +1,14 @@
+<?php
+  class Bureau{
+    private $role;
+    private $idAdh;
+
+    public function getRole(){
+      return $role;
+    }
+
+    public function getidAdh(){
+      return $idAdh;
+    }
+  }
+ ?>
