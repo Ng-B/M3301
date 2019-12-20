817fd7688a402db98564b1e8c4d4859ecbb45198
LARAICHI Aymane <laraicha@hostname.iut2.upmf-grenoble.fr>
2019-12-20 14:09:31 +0100

    tyuit

--- View/formulaire.View.php
+++ View/formulaire.View.php
@@ -113,7 +113,7 @@
 	           <input type="text" name="phoneMF" placeholder="0456358975"><br><br>
 	        </fieldset>
 	        </form><br>
-						<input type="submit" value="Valider" id="valider">
+						<a href="confirmation.View.php"><input type="submit" value="Valider" id="valider"></a>
 				</div>
 
 			</article>
