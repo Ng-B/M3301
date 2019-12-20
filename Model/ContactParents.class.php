894313a3dfd37c2d914da1e0ebcc9344e4a06776
LOCATELLI_Antoine <locatean@NTIUT2GRE.IUT2.UPMF-GRENOBLE.FR>
2019-12-20 14:16:05 +0100

    classe contact parents

---
+++ Model/ContactParents.class.php
@@ -0,0 +1,25 @@
+<?php
+  class ContactParents{
+    private $telPere;
+    private $fixePere;
+    private $telMere;
+    private $fixeMere;
+  }
+
+  public function getTelPere(){
+    return $telPere;
+  }
+
+  public function getFixePere(){
+    return $fixePere;
+  }
+
+  public function getTelMere(){
+    return $telMere;
+  }
+
+  public function getFixeMere(){
+    return $fixeMere;
+  }
+
+ ?>
