97f2935f458b177950105260f48438da9a70230e
Raymond <locatean@hostname.iut2.upmf-grenoble.fr>
2019-12-20 16:31:15 +0100

    wxdcfvgbhnjklm

---
+++ Model/Entraineur.class.php
@@ -0,0 +1,14 @@
+<?php
+  class Bureau{
+    private $categorie;
+    private $idAdh;
+
+    public function getCategorie(){
+      return $categorie;
+    }
+
+    public function getidAdh(){
+      return $idAdh;
+    }
+  }
+ ?>
