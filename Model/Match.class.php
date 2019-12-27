c3cadcb55b92f78431e04492e30199534b738968
Padir2 <locatelliraymond@gmail.com>
2019-12-27 13:14:07 +0100

    beflmizylg

---
+++ Model/Match.class.php
@@ -0,0 +1,33 @@
+<?php
+    class Match{
+      private $lieu;
+      private $date;
+      private $heure;
+      private $arbitre;
+      private $id;
+      private $categorie;
+
+      public function getId(){
+        return $id;
+      }
+
+      public function getLieu(){
+        return $lieu;
+      }
+
+      public function getDate(){
+        return $date;
+    }
+
+    public function getHeure(){
+      return $heure;
+    }
+
+    public function getArbitre(){
+      return $arbitre;
+    }
+
+    public function getCategorie(){
+      return $categorie;
+    }
+ ?>
