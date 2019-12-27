c220081f2abafac1cf3249962c9646272d87bd49
Padir2 <locatelliraymond@gmail.com>
2019-12-27 13:07:55 +0100

    zemfqgjù

---
+++ Model/Ratios.class.php
@@ -0,0 +1,24 @@
+<?php
+    class Ratios{
+      private $idJoueur;
+      private $idMatch;
+      private $nbButs;
+      private $nbTirs;
+
+      public function getIDJoueur(){
+        return $idJoueur;
+      }
+
+      public function getIDMatch(){
+        return $idMatch;
+      }
+
+      public function getNbButs(){
+        return $nbButs
+      }
+
+      public function getNbTirs(){
+        return $nbTirs;
+      }
+    }
+ ?>
