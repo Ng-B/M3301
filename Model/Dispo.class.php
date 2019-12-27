02debd8364bdbaf1fa66b8c52951e08a6f5fb984
Padir2 <locatelliraymond@gmail.com>
2019-12-27 12:56:31 +0100

    flqhmogn vhl

--- Model/Dispo.class.php
+++ Model/Dispo.class.php
@@ -1,4 +1,30 @@
 <?php
-    class Dispo
+    class Dispo{
+      private $idAdh;
+      private $idMatch;
+      private $peutEmmene;
+      private $estEmmene;
+      private $placeDispos;
+
+      public function getIdAdh(){
+        return $idAdh;
+      }
+
+      public function getIdMatch(){
+        return $idMatch;
+      }
+
+      public function isPeutEmmene(){
+        return $peutEmmene;
+      }
+
+      public function isEstEmmene(){
+        return $estEmmene;
+      }
+
+      public function getPlaceDispos(){
+        return $placeDispos;
+      }
+    }
 
  ?>
