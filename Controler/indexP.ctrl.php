8bf373318c83ccd97b3a8d83d54257308a589634
Ng-B <naodg68@gmail.com>
2020-01-09 15:11:58 +0100

    zs

--- Controler/indexP.ctrl.php
+++ Controler/indexP.ctrl.php
@@ -6,19 +6,7 @@ require_once("../Model/Adherent.class.php");
 include("../Model/DAO.classe.php");
 
 
-// Creation de l'instace DAO
-/*
-$adherents = new DAO("/users/info/etu-s3/bekelen/public_html/M3301/Model/data");
-$NTA=$adherents->getNombreTotalAdherents();
-echo $NTA;
-  for($i=1;$i<7;$i++){
-      // Récupération de tous les objets Music
-      $m = $adherents->getAdherentParId($i);
-      // Ajout à la liste des telephones à afficher
-      $list[$i] = $m;
-    }
-*/
-//---------------------------------------------Tri par prix, méthode TRIX PAR BULLE-------------------------------------//
+
 
 $view= new View("../View/indexP.View.php");
 //$view->list=$list;
--- Controler/main.ctrl.php
+++ Controler/main.ctrl.php
@@ -6,19 +6,7 @@ require_once("../Framework/view.class.php");
 include("../Model/DAO.classe.php");
 
 
-// Creation de l'instace DAO
-/*
-$adherents = new DAO("/users/info/etu-s3/bekelen/public_html/M3301/Model/data");
-$NTA=$adherents->getNombreTotalAdherents();
-echo $NTA;
-  for($i=1;$i<7;$i++){
-      // Récupération de tous les objets Music
-      $m = $adherents->getAdherentParId($i);
-      // Ajout à la liste des telephones à afficher
-      $list[$i] = $m;
-    }
-*/
-echo "Maneur";
+
 //---------------------------------------------Tri par prix, méthode TRIX PAR BULLE-------------------------------------//
 $view= new View("../View/lesmatchs.View.php");
 //$view->list=$list;
