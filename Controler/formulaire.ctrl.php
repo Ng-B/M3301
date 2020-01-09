c70140e30eeb3c2313442f1bfc971b548886d902
FABRY Dorian <fabryd@hostname.iut2.upmf-grenoble.fr>
2020-01-09 18:20:22 +0100

    er

--- Controler/formulaire.ctrl.php
+++ Controler/formulaire.ctrl.php
@@ -5,10 +5,6 @@ require_once("../Framework/view.class.php");
 require_once("../Model/Adherent.class.php");
 include("../Model/DAO.classe.php");
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 20c1ad49a1e7d9de90969b63b1a20898afd01a10
 $view= new View("../View/formulaire.View.php");
 
 $view->show();
