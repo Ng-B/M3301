cc0800e9604aa31dd6a2bbb97d303f4a46917eab
Ng-B <naodg68@gmail.com>
2020-01-10 14:25:02 +0100

    s

--- Model/DAO.classe.php
+++ Model/DAO.classe.php
@@ -269,7 +269,7 @@ public function getAdherentParDateNaiisance(string $date /*pas de variable date*
             if(isset($tell1) && isset($tell2)){
             $this->db->query("INSERT INTO ADHERENT VALUES ((SELECT MAX(id)+1 FROM ADHERENT ), '$nom', '$prenom', $dateNaissance, '$sexe','$telperso','$prenom.$nom','$nom.$dateNaissance',NULL,'$typelicense',false,'000',$mail,(SELECT id FROM Contact WHERE telpere=$tel1 AND telmere=$tel2 )");
           }
-        }.
+        }
 
           public function getAdherentParMail(string $mail) {
             $reponse=$this->db->query("SELECT * FROM ADHERENT WHERE mail='$mail' ");
