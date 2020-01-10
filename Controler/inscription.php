0c4ef9a089670625bab6cc5ae75e49ea009c29ce
FABRY Dorian <fabryd@hostname.iut2.upmf-grenoble.fr>
2020-01-10 15:30:56 +0100

    fghgzrhlsfgdqsfghdfgdqphuiofbdphuiosfdqsfgqsfqsfgdqzrgqjkozrgqo

--- Controler/inscription.php
+++ Controler/inscription.php
@@ -1,5 +1,5 @@
 <?php
-require_once('../Model/DAO.class.php');
+require_once("../Model/DAO.class.php");
 $lastname=$_POST["firstname"];
 $firstname=$_POST["lastname"];
 $date=$_POST["date"];
@@ -15,6 +15,7 @@ $phonePF=$_POST["phonePF"];
 $phoneM=$_POST["phoneM"];
 $phoneMF=$_POST["phoneMF"];
 if ($gender=="male"){
+
 ajoutAdherent($lastname, $firstname, $date, 1, $phone,$mail,$phoneP,$phonePF,$phoneM,$phoneMF,$joueur,$bureau,$entraineur,$categorie);
 }else{
 ajoutAdherent($lastname, $firstname, $date, 0, $phone,$mail,$phoneP,$phonePF,$phoneM,$phoneMF,$joueur,$bureau,$entraineur,$categorie);
