21071b9d871c1f3f3bef62880b9d1a6eec81e870
FABRY Dorian <fabryd@hostname.iut2.upmf-grenoble.fr>
2020-01-10 16:01:43 +0100

    ju

--- Model/DAO.class.php
+++ Model/DAO.class.php
@@ -189,7 +189,7 @@ public function getAdherentParDateNaissance(string $date /*pas de variable date*
         }
 
 
-        public function getContactParent(int $tel1,int $tel2): Contact {
+        public function getContactParent( $tel1, $tel2): Contact {
           $reponse=$this->db->query("SELECT * FROM Contact WHERE telPere=$tel1 AND telMere=$tel2 ");
           $donnees=$reponse-> fetchAll(PDO::FETCH_CLASS,"Contact");
            if(count($donnees) == 0) {
